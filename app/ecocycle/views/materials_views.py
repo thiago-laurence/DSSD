646b20dc233010a1bf2cc5ac9f6a0f2fcc1da769
Relderf <relderfeg@gmail.com>
2024-09-17 20:46:57 -0300

    fix: Material_view solo renderiza la página (sin form)

--- app/ecocycle/views/materials_views.py
+++ app/ecocycle/views/materials_views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, redirect
-#from .forms import MaterialForm
+from ..forms import RecoleccionForm
 
 def materials_new(request):
     #if request.method == 'POST':
-    #    form = MaterialForm(request.POST)
+    #    form = RecoleccionForm(request.POST)
     #    if form.is_valid():
     #        form.save()
     #        return redirect('/')
     #else:
-    #    form = MaterialForm()
+    #    form = RecoleccionForm()
     return render(request, 'materials/materials_new.html')#, {'form': form})
