fe66f22a9a36a3de02cdd278afd5af715b62e5fb
Relderf <relderfeg@gmail.com>
2024-09-17 14:07:23 -0300

    feat: forms.py con datos de Recoleccion

---
+++ app/ecocycle/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+from .models import Recoleccion
+
+class RecoleccionForm(forms.ModelForm):
+    class Meta:
+        model = Recoleccion
+        fields = ['id', 'id_recolector', 'id_local', 'semana', 'pago', 'observaciones']
+        
