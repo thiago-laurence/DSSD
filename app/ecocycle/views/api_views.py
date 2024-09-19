3a3ec20380906a7d81e188e4bb077eebd4821d02
Mateo Gmues <mateogmues@gmail.com>
2024-09-19 19:20:33 -0300

    fix: añado debugs

--- app/ecocycle/views/api_views.py
+++ app/ecocycle/views/api_views.py
@@ -4,8 +4,7 @@ from django.shortcuts import render
 
 def index(request):
     obj = obtener_procesos(request)
-
-    return render(request, 'api/index.html')
+    return render(request, 'api/index.html', {'procesos': obj})
 
 def login_bonita(request):
     url = "http://localhost:8080/bonita/loginservice"
@@ -38,10 +37,8 @@ def login_bonita(request):
 
 def get_bonita_tokens(request):
     tokens = request.session.get('bonita_tokens')
-    
     if not tokens:
         tokens = login_bonita(request)
-    
     return tokens
 
 def obtener_procesos(request):
@@ -52,7 +49,7 @@ def obtener_procesos(request):
             "Cookie": f"JSESSIONID={tokens['jsessionid']}",
             "X-Bonita-API-Token": tokens['api-token']
         }
-        url = "http://localhost:8080/bonita/API/living/application?p=0&c=10"
+        url = "http://localhost:8080/bonita/API/bpm/process?p=0&c=10"
         response = requests.get(url, headers=headers)
 
         if response.status_code == 200:
