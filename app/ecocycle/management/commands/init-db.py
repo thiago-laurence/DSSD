b8609d52ad13660fb487c7aabf4fbc2529623c2b
Relderf <relderfeg@gmail.com>
2024-09-27 00:04:46 -0300

    feat: Init-db agrega 3 usuarios de bonita

--- app/ecocycle/management/commands/init-db.py
+++ app/ecocycle/management/commands/init-db.py
@@ -29,9 +29,17 @@ class Command(BaseCommand):
     def create_example_data(self):
         # Recolectores
         r1 = Recolector.objects.create(nombre='John', apellido='Doe', email='johndoe@example.com', password='123')
+        # Bonitos
+        r2 = Recolector.objects.create(nombre='Walter', apellido='Bates', email='walter.bates@example.com', password='123', USERNAME_FIELD='walter.bates')
+        r3 = Recolector.objects.create(nombre='Jan', apellido='Fisher', email='jan.fisher@example.com', password='123', USERNAME_FIELD='jan.fisher')
+        r4 = Recolector.objects.create(nombre='William', apellido='Jobs', email='william.jobs@example.com', password='123', USERNAME_FIELD='william.jobs')
 
         # Puntos
         p1 = Punto.objects.create(nombre='Punto 1', direccion='Calle 1', password='123', email='punto1@example.com')
+        p2 = Punto.objects.create(nombre='Punto 2', direccion='Calle 2', password='123', email='punto2@example.com')
 
         # Asignacion de Puntos a Recolectores
         r1.puntos.add(p1)
+        r2.puntos.add(p1)
+        r3.puntos.add(p2)
+        r4.puntos.add(p2)
