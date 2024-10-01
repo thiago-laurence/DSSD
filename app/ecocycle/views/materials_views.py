44a38016751ce3cc980196fe6ff2b892da3d06e9
Mateo <mateogmues@gmail.com>
2024-10-01 13:41:53 -0300

    feat: permisos para entrar al formulario de recoleccion

--- app/ecocycle/views/materials_views.py
+++ app/ecocycle/views/materials_views.py
@@ -3,6 +3,8 @@ from . import api_views
 from ..forms import RecoleccionForm
 
 def index(request):
+    if 'user' not in request.session:
+        return redirect('login:index')
     return render(request, 'materials/index.html')
 
 def send(request):
