577535130dd8aa155c8a5028cc358146f9c0b828
Relderf <relderfeg@gmail.com>
2024-10-10 15:41:53 -0300

    fix: Simplificación de add_pedido

--- app/ecocycle/views/api_views.py
+++ app/ecocycle/views/api_views.py
@@ -27,38 +27,30 @@ def get_pedidos(request):
     return JsonResponse(list(pedidos), safe=False)
 
 @csrf_exempt
+@api_view(['GET'])
 def add_pedido(request):
-    if request.method == 'POST':
-        try:
-            data = json.loads(request.body)
-            deposito_id = data['deposito']
-            material_nombre = data['material']
-            cantidad = data['cantidad']
-            
-            # Fetch Deposito object
-            try:
-                deposito = Deposito.objects.get(id=deposito_id)
-            except Deposito.DoesNotExist:
-                return JsonResponse({'error': 'Deposito not found'}, status=404)
+    data = json.loads(request.body)
+    deposito_id = data['deposito']
+    material_nombre = data['material']
+    cantidad = data['cantidad']
+    
+    # Fetch Deposito object
+    try:
+        deposito = Deposito.objects.get(id=deposito_id)
+    except Deposito.DoesNotExist:
+        return JsonResponse({'error': 'Deposito not found'}, status=404)
 
-            # Fetch Material object
-            try:
-                material = Material.objects.get(nombre=material_nombre)
-            except Material.DoesNotExist:
-                return JsonResponse({'error': 'Material not found'}, status=404)
+    # Fetch Material object
+    try:
+        material = Material.objects.get(nombre=material_nombre)
+    except Material.DoesNotExist:
+        return JsonResponse({'error': 'Material not found'}, status=404)
 
-            # Create the Pedido object
-            pedido = Pedido.objects.create(
-                deposito=deposito,
-                centro=None,
-                material=material,
-                cantidad=cantidad
-            )
-            return JsonResponse({'message': 'Pedido created successfully', 'pedido_id': pedido.id}, status=201)
-
-        except Material.DoesNotExist:
-            return JsonResponse({'error': 'Material not found'}, status=404)
-        except KeyError:
-            return JsonResponse({'error': 'Invalid input data'}, status=400)
-    else:
-        return JsonResponse({'error': 'POST request required'}, status=405)
+    # Create the Pedido object
+    pedido = Pedido.objects.create(
+        deposito=deposito,
+        centro=None,
+        material=material,
+        cantidad=cantidad
+    )
+    return JsonResponse({'message': 'Pedido created successfully', 'pedido_id': pedido.id}, status=201)
