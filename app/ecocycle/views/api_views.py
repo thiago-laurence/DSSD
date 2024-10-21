f7e07d09276ae06104724fbf6d3fef3479e83bed
Thiago Laurence <francolaurence47@gmail.com>
2024-10-21 07:36:12 -0300

    docs(openapi): actualizacion de documento Swagger

--- app/ecocycle/views/api_views.py
+++ app/ecocycle/views/api_views.py
@@ -5,15 +5,12 @@ from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
 from rest_framework.pagination import PageNumberPagination
 from ..models.deposito import Deposito
 from ..models.pedido import Pedido
 from ..models.material import Material
-from ..models.recolector import Recolector
 from ..models.centro import Centro
 from ..serializers.centro import CentroSerializer
-from ..serializers.recolector import RecolectorSerializer
 from ..serializers.pedido import PedidoSerializer
 
 @api_view(['GET'])
@@ -36,7 +33,7 @@ def add_pedido(request):
     try:
         cantidad = Decimal(cantidad)
     except:
-        return JsonResponse({"error": "Cantidad debe ser un número decimal."}, status=status.HTTP_400_BAD_REQUEST)
+        return JsonResponse({"error": "El campo Cantidad debe ser un número decimal."}, status=status.HTTP_400_BAD_REQUEST)
     
     pedido = Pedido.objects.create(
         deposito=deposito,
@@ -59,7 +56,7 @@ def get_centros(request):
     try:
         cantidad = Decimal(cantidad)
     except:
-        return JsonResponse({"error": "Cantidad debe ser un número decimal."}, status=status.HTTP_400_BAD_REQUEST)
+        return JsonResponse({"error": "El campo Cantidad debe ser un número decimal."}, status=status.HTTP_400_BAD_REQUEST)
 
     centros = Centro.objects.all().filter(
         centromaterial__material=material,
@@ -77,23 +74,6 @@ def get_centros(request):
 @csrf_exempt
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
-def asignar_pedido(request):
-    pedido_id = request.POST.get('pedido_id')
-    if pedido_id:
-        pedido = Pedido.objects.get(id=pedido_id)
-        centro_asignado = Centro.objects.get(id=request.session['user']['id'])
-        hay_materiales = centro_asignado.has_enough_material(pedido.material, pedido.cantidad)
-        if hay_materiales:
-            pedido.centro = centro_asignado
-            pedido.save()
-            return JsonResponse({"message": "Pedido tomado correctamente."}, status=200)
-        else: 
-            return JsonResponse({"error": "No hay suficiente material en el centro de acopio."}, status=400)
-    else:
-        return JsonResponse({"error": "ID de pedido no proporcionado."}, status=400)
-
-@csrf_exempt
-@api_view(['POST'])
 def add_deposito(request):
     nombre = request.data.get('nombre')
     direccion = request.data.get('direccion')
@@ -104,7 +84,7 @@ def add_deposito(request):
         return JsonResponse({"error": "Nombre, dirección, email y password son campos obligatorios."}, status=status.HTTP_400_BAD_REQUEST)
 
     try:
-        deposito = Deposito.objects.create( nombre=nombre, direccion=direccion, email=email, password=password)
+        Deposito.objects.create( nombre=nombre, direccion=direccion, email=email, password=password)
     except Exception as e:
         return JsonResponse({"error": "El email del deposito ya fue registrado."}, status=status.HTTP_409_CONFLICT)
     
